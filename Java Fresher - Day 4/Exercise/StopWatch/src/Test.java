1243053370aadaaf877c5040c8933021e15942c8
Loc <locnguyen5801@gmail.com>
2022-07-21 08:16:33 +0700

    Fix Stopwatch

--- Java Fresher - Day 4/Exercise/StopWatch/src/Test.java
+++ Java Fresher - Day 4/Exercise/StopWatch/src/Test.java
@@ -3,17 +3,36 @@ import java.lang.*;
 
 public class Test {
 
-    private static int Fibonacci(int n) {
-        if (n < 2)
-            return 1;
-        else
-            return Fibonacci(n - 1) + (Fibonacci(n - 2));
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j; //searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
     }
 
     public static void main(String[] args) {
         StopWatch watch = new StopWatch();
         watch.start();
-        System.out.println(Fibonacci(45));
+
+        // Seclection sort cho 100000 phan tu random
+        int n = 100000;
+        Random rnum = new Random();
+        int[] arr = new int[n];
+
+        for (int count = 0; count < n; count++){
+            arr[count] = rnum.nextInt();
+        }
+        selectionSort(arr);
+
+
         watch.end();
         System.out.println("Time has passed " + watch.getElapsedTime() + " milisecond" );
     }
